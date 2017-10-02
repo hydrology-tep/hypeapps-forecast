801762fef906f4941f7303df41ff3c4b787f300d
David Gustafsson <david.gustafsson@smhi.se>
2017-10-02 18:26:12 +0200

    pom.xml and cairo-env dependencies

---
+++ src/main/app-resources/scripts/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/opt/anaconda/bin/conda install -y --file /application/dependencies/R/packages.list
+#/opt/anaconda/bin/conda create --name cairo-env r-cairo
+/opt/anaconda/bin/conda create --name cairo-env --file /application/dependencies/R/cairo-env.list
