714569dbcfd2bb2683742a10f3a70f2d11196126
Tobias Lagander <tobias.lagander@smhi.se>
2020-05-11 08:35:12 +0200

    Check with PATH

--- src/main/app-resources/scripts/postinstall.sh
+++ src/main/app-resources/scripts/postinstall.sh
@@ -17,25 +17,32 @@
 # ghostscript-fonts.noarch             5.50-23.2.el6              @base
 # urw-fonts.noarch                     2.4-11.el6                 @base
 
-SUDO=/usr/bin/sudo
+export PATH=/opt/anaconda/bin/:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
+
+logfile=/home/tobiaslagander/log-mvn.txt
+
+SUDO=/usr/bin/sudo 
 YUM=/usr/bin/yum
 GREP=/bin/grep
 CUT=/bin/cut
 
-#${SUDO} ${YUM} downgrade -y proj
+${SUDO} ${YUM} downgrade -y proj
+echo $? > $logfile
 
-exp_proj_ver="4.7.0-2.el6"
-#exp_proj_ver="4.8.0-2.rhel6"
+echo $PATH >> $logfile
 
-proj_ver=$(${YUM} list installed | ${GREP} -i proj | ${CUT} -d ' ' -f 27)
+# exp_proj_ver="4.7.0-2.el6"
+# #exp_proj_ver="4.8.0-2.rhel6"
 
-if [ -n "${proj_ver}" ]; then
+# proj_ver=$(${YUM} list installed | ${GREP} -i proj | ${CUT} -d ' ' -f 27)
 
-    if [ "x${proj_ver}" != "x${exp_proj_ver}" ]; then
-	#${SUDO} ${YUM} install -y proj-${exp_proj_ver}
-	${SUDO} ${YUM} downgrade -y proj
-    fi
-fi
+# if [ -n "${proj_ver}" ]; then
+
+#     if [ "x${proj_ver}" != "x${exp_proj_ver}" ]; then
+# 	#${SUDO} ${YUM} install -y proj-${exp_proj_ver}
+# 	${SUDO} ${YUM} downgrade -y proj
+#     fi
+# fi
 
 /opt/anaconda/bin/conda install -y --file /application/dependencies/R/packages.list
 /opt/anaconda/bin/conda create --name cairo-env --file /application/dependencies/R/cairo-env.list
