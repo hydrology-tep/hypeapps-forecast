ac21b96e552170f1caacebf8e591e1214229cb6d
Tobias Lagander <tobias.lagander@smhi.se>
2020-05-13 09:43:50 +0200

    Test n+2

--- src/main/app-resources/scripts/postinstall.sh
+++ src/main/app-resources/scripts/postinstall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+SUDO=/usr/bin/sudo 
+YUM=/usr/bin/yum
+#GREP=/bin/grep
+#CUT=/bin/cut
+
+${SUDO} ${YUM} downgrade -y proj
+
 /opt/anaconda/bin/conda install -y --file /application/dependencies/R/packages.list
 /opt/anaconda/bin/conda create --name cairo-env --file /application/dependencies/R/cairo-env.list
